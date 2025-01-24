baf224bc1c69389552a7dd9dcf8b24f1f71ea023
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-24 08:52:22 +0100

    EXP2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hola Pull request!");
         System.out.println("Nueva Caracteristica");
+        System.out.println("Finalizada caracteristica");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
